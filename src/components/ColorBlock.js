4c1e519958fc22ee69a8079b22f3ebd81b82df67
Ddollz <44191473+Ddollz@users.noreply.github.com>
2022-02-26 16:27:30 +0800

    Card Color Copy

--- src/components/ColorBlock.js
+++ src/components/ColorBlock.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Color from "color"
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const ColorBlock = ({color}) => {
+const ColorBlock = ({ color }) => {
     return (
-        <div className="color-card" style={{background: color}}></div>
+        <CopyToClipboard text={Color(color).hex()}>
+            <div className="color-card" style={{ background: color }}>
+                <div className="ColorBlockCode">{Color(color).hex()}</div>
+            </div>
+        </CopyToClipboard>
     );
 }
 
--- src/components/PalleteList.js
+++ src/components/PalleteList.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import {AiOutlineMinusCircle} from 'react-icons/ai'
+import { AiOutlineMinusCircle } from 'react-icons/ai'
+import ColorBlock from './ColorBlock.js'
 
-function PalleteList({ Pallete_List,handleRemove }) {
+function PalleteList({ Pallete_List, handleRemove }) {
     return Pallete_List.map((pallete, index) => (
-        <div className="card-row" key={index}>
+        <div className="card-row" key={index} style={{ color: pallete.pallete.color3 }}>
             <div className="icon">
-            <AiOutlineMinusCircle onClick={()=>handleRemove(pallete.id)} className='delete-icon' style={{ color: pallete.pallete.color3 }}/>
+                <AiOutlineMinusCircle onClick={() => handleRemove(pallete.id)} className='delete-icon' />
             </div>
-            <div className="color-card" style={{ background: pallete.pallete.color1 }}></div>
-            <div className="color-card" style={{ background: pallete.pallete.color2 }}></div>
-            <div className="color-card" style={{ background: pallete.pallete.color3 }}></div>
-            <div className="color-card" style={{ background: pallete.pallete.color4 }}></div>
-            <div className="color-card" style={{ background: pallete.pallete.color5 }}></div>
+                <ColorBlock color={pallete.pallete.color1} />
+                <ColorBlock color={pallete.pallete.color2} />
+                <ColorBlock color={pallete.pallete.color3} />
+                <ColorBlock color={pallete.pallete.color4} />
+                <ColorBlock color={pallete.pallete.color5} />
+
         </div>
     ))
 
