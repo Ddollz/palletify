eb0bd65eaa0bfbfbd03c4baa1f5ac0cdf6814b73
Ddollz <44191473+Ddollz@users.noreply.github.com>
2022-02-26 16:42:55 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => (
   <div className="footer">
-    <a href='https://github.com/Ddollz'><h1>Palletify</h1></a>&nbsp; · &nbsp;made by <a href='https://github.com/Ddollz' target='_blank' rel='noopener noreferrer'>Karl</a>&nbsp; · &nbsp;<a href='https://github.com/hihayk/scale' target='_blank' rel='noopener noreferrer'>GitHub</a>
+    <a href='https://github.com/Ddollz'><h1>Palletify</h1></a>&nbsp; · &nbsp;made by <a href='https://github.com/Ddollz' target='_blank' rel='noopener noreferrer'>Karl</a>&nbsp; · &nbsp;<a href='https://github.com/Ddollz' target='_blank' rel='noopener noreferrer'>GitHub</a>
   </div>
 )
 
